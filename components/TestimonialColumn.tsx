f06d814f883ce85ece6131702aacfabb912a6c57
Sujay Rajesh <sujay.rajesh08@gmail.com>
2025-06-20 10:41:11 -0700

    refactor: Remove unused image property from testimonials and clean up rendering logic

--- components/TestimonialColumn.tsx
+++ components/TestimonialColumn.tsx
@@ -8,7 +8,6 @@ type TestimonialProps = {
   className?: string;
   testimonials: {
     text: string;
-    image: string;
     name: string;
     role: string;
     rating: number;
@@ -34,33 +33,31 @@ export const TestimonialsColumn = (props: TestimonialProps) => {
         {[
           ...new Array(2).fill(0).map((_, index) => (
             <React.Fragment key={index}>
-              {props.testimonials.map(
-                ({ text, image, name, role, rating }, i) => (
-                  <div
-                    className="p-10 pt-8 rounded-3xl shadow-2xl lg shadow-primary/15 max-w-xs w-full"
-                    key={i}
-                  >
-                    <div className="flex items-center gap-1 mb-4">
-                      {[...Array(rating)].map((_, i) => (
-                        <Star
-                          key={i}
-                          className="w-5 h-5 text-yellow-400 fill-yellow-400"
-                        />
-                      ))}
-                    </div>
-                    <p className="font-semibold">&quot;{text}&quot;</p>
+              {props.testimonials.map(({ text, name, role, rating }, i) => (
+                <div
+                  className="p-10 pt-8 rounded-3xl shadow-2xl lg shadow-primary/15 max-w-xs w-full"
+                  key={i}
+                >
+                  <div className="flex items-center gap-1 mb-4">
+                    {[...Array(rating)].map((_, i) => (
+                      <Star
+                        key={i}
+                        className="w-5 h-5 text-yellow-400 fill-yellow-400"
+                      />
+                    ))}
+                  </div>
+                  <p className="font-semibold">&quot;{text}&quot;</p>
 
-                    <div className="flex flex-col mt-5">
-                      <div className="font-bold tracking-tight leading-5">
-                        {name}
-                      </div>
-                      <div className="leading-5 font-semibold opacity-60 tracking-tight">
-                        {role}
-                      </div>
+                  <div className="flex flex-col mt-5">
+                    <div className="font-bold tracking-tight leading-5">
+                      {name}
+                    </div>
+                    <div className="leading-5 font-semibold opacity-60 tracking-tight">
+                      {role}
                     </div>
                   </div>
-                ),
-              )}
+                </div>
+              ))}
             </React.Fragment>
           )),
         ]}
