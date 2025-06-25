2464774f64ba5023fbf5e8a03aaf0a246df746a2
Sujay Rajesh <sujay.rajesh08@gmail.com>
2025-06-24 23:24:34 -0700

    fix: mobile responsiveness bug

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Authenticated, Unauthenticated } from "convex/react";
-import { Plus } from "lucide-react";
+import { Link2, Plus } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
@@ -11,24 +11,21 @@ import { AuroraText } from "./magicui/aurora-text";
 function Header() {
   return (
     <header className="pt-2">
-      <div className="max-w-7xl mx-auto px-12 py-4 flex justify-between items-center font-satoshi font-extrabold">
-        <div className="hidden sm:flex items-center h-full gap-3 md:gap-10 text-gray-900">
-          <Link href="/dashboard" className="text-xl">
-            Dashboard
-          </Link>
-          <Link href="/billing" className="text-xl">
-            Billing
-          </Link>
-        </div>
-        <Link href="/" className="sm:text-3xl text-2xl text-center font-black">
-          <AuroraText>LinkGrove</AuroraText>
-        </Link>
-
+      <div className="max-w-7xl mx-auto py-4 px-5 sm:px-12 flex justify-between items-center font-satoshi font-extrabold">
         <Authenticated>
-          <div className="flex gap-5 items-center bg-white/50 backdrop-blur-sm border border-white/20 rounded-lg">
+          <Link href="/">
+            <div className="hidden sm:flex">
+              <AuroraText className="sm:text-3xl text-center font-black">
+                LinkGrove
+              </AuroraText>
+            </div>
+
+            <Link2 className="h-9 w-9 bg-gradient-to-r from-blue-500 to-purple-600 text-white p-2 rounded-full sm:hidden" />
+          </Link>
+          <div className="flex gap-1.5 sm:gap-5 items-center bg-white/50 backdrop-blur-sm border border-white/20 rounded-lg">
             <Link
               href="/dashboard/new-link"
-              className="inline-flex items-center gap-1 bg-gradient-to-r from-blue-500 to-purple-600 text-white px-3 py-2 rounded-lg font-bold hover:from-blue-600 hover:to-purple-780 transition-all duration-200"
+              className="inline-flex items-center gap-1 bg-gradient-to-r from-blue-500 to-purple-600 text-white px-3 py-2 text-sm sm:text-base rounded-lg font-bold hover:from-blue-600 hover:to-purple-780 transition-all duration-200"
             >
               <Plus className="w-4 h-4" />
               Add Link
@@ -36,7 +33,7 @@ function Header() {
             <Button
               asChild
               variant="outline"
-              className="border-purple-600 px-5 py-[19px] font-bold text-purple-600 hover:border-purple-700 hover:bg-purple-600 hover:text-white transition-all duration-200 text-base size52"
+              className="border-purple-600 px-5 py-[19px] font-bold text-purple-600 hover:border-purple-700 hover:bg-purple-600 hover:text-white transition-all duration-200 text-sm sm:text-base"
             >
               <Link href="/billing">Billing</Link>
             </Button>
@@ -45,6 +42,20 @@ function Header() {
         </Authenticated>
 
         <Unauthenticated>
+          <div className="hidden sm:flex items-center h-full gap-3 md:gap-10 text-gray-900">
+            <Link href="/dashboard" className="text-xl">
+              Dashboard
+            </Link>
+            <Link href="/billing" className="text-xl">
+              Billing
+            </Link>
+          </div>
+          <Link
+            href="/"
+            className="sm:text-3xl text-2xl text-center font-black"
+          >
+            <AuroraText>LinkGrove</AuroraText>
+          </Link>
           <SignInButton mode="modal">
             <Button
               variant="outline"
